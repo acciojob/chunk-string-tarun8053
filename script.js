fb61bc7b1f469a72a366b1c607571e219dbdc510
Tarun <thakurtarun1012@gmail.com>
2025-09-26 13:25:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function stringChop(str, size) {
-  // your code here
+function stringChop(str, len) {
+  let arr = [];
+  for (let i = 0; i < str.length; i += len) {
+    arr.push(str.substring(i, i + len));
+  }
+  return arr;
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
+const str = prompt("Enter String:");
+const size = parseInt(prompt("Enter Chunk Size:"), 10); // convert to number
 alert(stringChop(str, size));
